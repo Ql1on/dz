c42a86aa93058a35e1fe568bf037478a77ba2fc6
vadim <vadimeray2006@gmail.com>
2022-10-13 16:36:16 +0800

    second task is complited

--- ZADACHI/seminar1/Program.cs
+++ ZADACHI/seminar1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задача 1
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+Console.WriteLine("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+bool A = n1 == n2 * n2; //n1 в квадрате равен n2*n2
+Console.WriteLine(A);
+//решено!!!!
+
+*/
+
+/* Задача 2
+Console.Write("Input a positive integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = -n;
+while (current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
