dbdae1902f9824dcd5ee92a2214dad66c140a9e4
vadim <vadimeray2006@gmail.com>
2022-10-25 15:56:29 +0800

    first task complited

--- ZADACHI/seminar3/Program.cs
+++ ZADACHI/seminar3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+bool PalindromCheck (int num) 
+{
+    int help = 0;
+    int result = 0;
+    int i = 0;
+    int help2 = num;
+    while (i < 5)
+    {
+        help = help2 % 10;
+        help2 = help2 / 10;
+        result = (result + help) * 10;
+        i++;
+    }
+    result = result / 10;
+
+
+    Console.WriteLine($"result{result}");
+    Console.WriteLine($"num {num}");
+    if (num == result) return true;
+    else return false;
+
+}
+int a = Convert.ToInt32(Console.ReadLine());
+bool r = PalindromCheck(a);
+Console.WriteLine(r);
+*/
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
