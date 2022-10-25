67b1dde27650962c61a9922514aea6d439b4d2cc
vadim <vadimeray2006@gmail.com>
2022-10-25 16:15:52 +0800

    second task complited

--- ZADACHI/seminar3/Program.cs
+++ ZADACHI/seminar3/Program.cs
@@ -28,5 +28,24 @@ bool r = PalindromCheck(a);
 Console.WriteLine(r);
 */
 
+
+/*
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+void DistanceIn3D (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)),2);
+    Console.WriteLine($"Расстояние между точками в 3D: {distance}");
+}
+
+Console.WriteLine("Введите три координаты первой точки");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите три координаты второй точки");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+DistanceIn3D(x1,y1,z1,x2,y2,z2);
+*/
 
